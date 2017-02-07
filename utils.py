d54f5d5175a6a0261e0f96d2aebb8c6919e8a210
Peter Ale <Peter.Ale@veritas.com>
2017-02-07 17:15:27 +0800

    timer and checksum

---
+++ utils.py
@@ -0,0 +1,34 @@
+import time as t
+
+
+class Timer:
+    def __enter__(self):
+        self.begin = t.time()
+        return self
+
+    def __exit__(self, *args):
+        self.duration = t.time() - self.begin
+
+
+'''
+Return the checksum of the given data.
+The algorithm comes from:
+http://en.wikipedia.org/wiki/IPv4_header_checksum
+'''
+
+
+def checksum(data):
+    data_sum = 0
+    # pick up 16 bits (2 WORDs) every time
+    for i in range(0, len(data), 2):
+        # Sum up the ordinal of each WORD with
+        # network bits order (big-endian)
+        if i < len(data) and (i + 1) < len(data):
+            data_sum += (ord(data[i]) + (ord(data[i + 1]) << 8))
+        elif i < len(data) and (i + 1) == len(data):
+            data_sum += ord(data[i])
+    add_on_carry = (data_sum & 0xffff) + (data_sum >> 16)
+    result = (~ add_on_carry) & 0xffff
+    # swap bytes
+    result = result >> 8 | ((result & 0x00ff) << 8)
+    return result
