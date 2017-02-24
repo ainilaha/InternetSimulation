eb24baa241fce4bf476a14f0af776489d72aca88
Peter Ale <Peter.Ale@veritas.com>
2017-02-25 01:11:25 +0800

    revised __repr_ function and test pack and unpack

--- rip.py
+++ rip.py
@@ -2,11 +2,11 @@
 import socket
 from struct import pack, unpack, calcsize
 
-RIP_HDR_FMT = '!BBB4sB'
+RIP_HDR_FMT = '!BBBH4s'
 
 
 class RIPPacket:
-    def __init__(self, cmd, ver=1, afi=2, ip_addr='', metric=0):
+    def __init__(self, cmd=0, ver=1, afi=2, ip_addr='', metric=0):
         # RIP head:
         self.cmd = cmd  # request or response
         self.ver = ver
@@ -16,28 +16,33 @@ class RIPPacket:
         self.metric = metric
 
     def __repr__(self):
-        rep = 'RIPPacket: [cmd:%d, ver:%d, afi:%d, addr:%s, metric:%d]' \
-              % (self.cmd, self.ver, self.afi, socket.inet_ntoa(self.ip_addr), self.metric)
+        rep = 'RIPPacket: [cmd:%s, ver:%d, afi:%d, addr:%s, metric:%d]' \
+              % ('request' if self.cmd == 0 else "response", self.ver, self.afi,
+                 socket.inet_ntoa(self.ip_addr), self.metric)
         return rep
 
     def pack(self):
         rip_packet = pack(RIP_HDR_FMT,
-                          self.cmd, self.ver, self.afi, self.ip_addr, self.metric)
+                          self.cmd, self.ver, self.afi, self.metric, self.ip_addr)
         return rip_packet
 
-    def unpack(self, rip_packets):
-        rip_fields = unpack(RIP_HDR_FMT, rip_packets[:calcsize(RIP_HDR_FMT)])
+    def unpack(self, rip_packet):
+        rip_fields = unpack(RIP_HDR_FMT, rip_packet[:calcsize(RIP_HDR_FMT)])
         self.cmd = rip_fields[0]
         self.ver = rip_fields[1]
         self.afi = rip_fields[2]
-        self.ip_addr = rip_fields[3]
-        self.metric = rip_fields[4]
+        self.metric = rip_fields[3]
+        self.ip_addr = rip_fields[4]
+
 
 def main():
     ip = "10.10.10.11"
     ip_p = socket.inet_aton(ip)
-    rip = RIPPacket(cmd=1, ip_addr=ip_p)
-    print  rip.__repr__()
+    rip = RIPPacket(cmd=0, ip_addr=ip_p)
+    print "rip1=" + rip.__repr__()
+    rip2 = RIPPacket(cmd=0, ip_addr="")
+    rip2.unpack(rip.pack())
+    print "rip2=" + rip2.__repr__()
 
 
 if __name__ == "__main__":
